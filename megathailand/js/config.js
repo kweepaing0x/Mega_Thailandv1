ad5252900508213019a5f0349b86b69759ac4db0
kweepaing0x <166644326+kweepaing0x@users.noreply.github.com>
2026-04-24 16:38:15 +0700

    Update config.js

--- megathailand/js/config.js
+++ megathailand/js/config.js
@@ -6,4 +6,4 @@ const SUPABASE_URL = "https://lcxiilnovbzytkvkzrfq.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxjeGlpbG5vdmJ6eXRrdmt6cmZxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwMTQ0NDAsImV4cCI6MjA5MjU5MDQ0MH0.MHqm0SyoVAc4xureTM37L0mS4XJ6aakywemZ9ckqPbo";
 // ============================================================
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
